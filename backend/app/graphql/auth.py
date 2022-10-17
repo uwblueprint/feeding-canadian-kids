325459848d4117fe5604bac56e1705d1507cf2d9
Alyssa Guo <alyssasuiyiguo@gmail.com>
2022-10-16 21:55:06 -0400

    Added error handling

--- backend/app/graphql/auth.py
+++ backend/app/graphql/auth.py
@@ -37,6 +37,9 @@ cookie_options = {
 blueprint = Blueprint("auth", __name__, url_prefix="/auth")
 
 class Login(Mutation):
+    """
+    Returns access token in response body and sets refreshToken as an httpOnly cookie
+    """
     class Arguments:
         email = graphene.String()
         password = graphene.String()
@@ -49,62 +52,94 @@ class Login(Mutation):
     role = graphene.String()
 
     def mutate(self, info, code, email, password):
+        try:
+            auth_dto = None
+            if "id_token" in request.json:
+                auth_dto = auth_service.verify_token(request.json["id_token"])
+            else:
+                auth_dto = auth_service.generate_token(request.json["email"], request.json["password"])
 
-        auth_dto = None
-        if "id_token" in request.json:
-            auth_dto = auth_service.verify_token(request.json["id_token"])
-        else:
-            auth_dto = auth_service.generate_token(request.json["email"], request.json["password"])
+            # middleware for set cookie
+            info.context.set_cookie = code
 
-        # middleware for set cookie
-        info.context.set_cookie = code
-
-        return Login(access_token=auth_dto.access_token, id=auth_dto.id, first_name=auth_dto.first_name, last_name=auth_dto.last_name, email=auth_dto.email, role=auth_dto.role)
+            return Login(access_token=auth_dto.access_token, id=auth_dto.id, first_name=auth_dto.first_name, last_name=auth_dto.last_name, email=auth_dto.email, role=auth_dto.role)
+        except Exception as e:
+            error_message = getattr(e, "message", None)
+            return jsonify({"error": (error_message if error_message else str(e))}), 500
 
 
 class Register(Mutation):
+    """
+    Returns access token and user info in response body and sets refreshToken as an httpOnly cookie
+    """
     def mutate(self, info, code, email, password):
-        request.json["role"] = "User"
-        user = CreateUserDTO(**request.json)
-        user_service.create_user(user)
-        auth_dto = auth_service.generate_token(
-            request.json["email"], request.json["password"]
-        )
+        try:
+            request.json["role"] = "User"
+            user = CreateUserDTO(**request.json)
+            user_service.create_user(user)
+            auth_dto = auth_service.generate_token(
+                request.json["email"], request.json["password"]
+            )
 
-        auth_service.send_email_verification_link(request.json["email"])
+            auth_service.send_email_verification_link(request.json["email"])
 
-        info.context.set_cookie = code
+            info.context.set_cookie = code
 
-        return Login(access_token=auth_dto.access_token, id=auth_dto.id, first_name=auth_dto.first_name, last_name=auth_dto.last_name, email=auth_dto.email, role=auth_dto.role)
-
+            return Login(access_token=auth_dto.access_token, id=auth_dto.id, first_name=auth_dto.first_name, last_name=auth_dto.last_name, email=auth_dto.email, role=auth_dto.role)
+        except Exception as e:
+            error_message = getattr(e, "message", None)
+            return jsonify({"error": (error_message if error_message else str(e))}), 500
 
 
 class Refresh(Mutation):
+    """
+    Returns access token in response body and sets refreshToken as an httpOnly cookie
+    """
     class Arguments:
         user_id = graphene.String()
 
     access_token = graphene.String()
 
     def mutate(self, info, code, user_id):
-        token = auth_service.renew_token(request.cookies.get("refreshToken"))
-        # middleware for set cookie
-        info.context.set_cookie = code
-        return Refresh(access_token=token)
+        try:
+            token = auth_service.renew_token(request.cookies.get("refreshToken"))
+            # middleware for set cookie
+            info.context.set_cookie = code
+            return Refresh(access_token=token)
+        except Exception as e:
+            error_message = getattr(e, "message", None)
+            return jsonify({"error": (error_message if error_message else str(e))}), 500
 
 
 class Logout(Mutation):
+    """
+    Revokes all of the specified user's refresh tokens
+    """
     class Arguments:
         user_id = graphene.String()
 
     def mutate(self, info, user_id):
-        auth_service.revoke_tokens(user_id)
-        return Logout()
+        try:
+            auth_service.revoke_tokens(user_id)
+            return Logout()
+        except Exception as e:
+            error_message = getattr(e, "message", None)
+            return jsonify({"error": (error_message if error_message else str(e))}), 500
+
 
 
 class ResetPassword(Mutation):
+    """
+    Triggers password reset for user with specified email (reset link will be emailed)
+    """
     class Arguments:
         email = graphene.String()
 
     def mutate(self, info, email):
-        auth_service.reset_password(email)
-        return ResetPassword()
+        try:
+            auth_service.reset_password(email)
+            return ResetPassword()
+        except Exception as e:
+            error_message = getattr(e, "message", None)
+            return jsonify({"error": (error_message if error_message else str(e))}), 500
+

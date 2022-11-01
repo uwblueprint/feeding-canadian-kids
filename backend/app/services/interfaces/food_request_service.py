e4e92de0ec2c4a2019e29c22c9225981fe5da560
Philips Xu <philips.ilips@gmail.com>
2022-11-01 17:59:45 -0400

    wip

--- backend/app/services/interfaces/food_request_service.py
+++ backend/app/services/interfaces/food_request_service.py
@@ -7,11 +7,47 @@ class IFoodRequestService(ABC):
     """
 
     @abstractmethod
-    def create_food_request_group(self, food_request_details):
+    def create_food_request_group(self, description, requestor, commitments):
         """Create a new FoodRequestGroup object and corresponding FoodRequests
 
-        :param food_request_details: recipient's email
-        :type food_request_details: dict containing fields like description,
+        :param description: description of the food request group
+        :type description: string
+        :param requestor: ID of the ASP user who is requesting food 
+        :type requestor: ObjectID 
+        :param commitments: list of dates and meal types for the food request group
+        :type commitments: list
+        :return: dict of FoodRequestGroup object
+        :rtype: dict
+        :raises Exception: if FoodRequestGroup was not created successfully
+        """
+        pass
+
+    @abstractmethod
+    def get_food_request_groups_by_user(self, user_id, is_impersonation):
+        """Get a user's FoodRequestGroup objects and corresponding FoodRequests
+
+        :param user_id: ID of the ASP user 
+        :type user_id: ObjectID 
+        :param is_impersonation: indicates whether the user is being impersonated by admin
+        :type is_impersonation: bool 
+        :return: list of of FoodRequestGroup dicts 
+        :rtype: list 
+        :raises Exception: if FoodRequestGroups were not fetched successfully
+        """
+        pass
+
+
+    @abstractmethod
+    # only called by admins
+    def get_food_request_groups(self):
+        """Get list of all FoodRequestGroup objects and corresponding FoodRequests
+
+        :param description: description of the food request group
+        :type description: string
+        :param requestor: ID of the ASP user who is requesting food 
+        :type requestor: ObjectID 
+        :param commitments: list of dates and meal types for the food request group
+        :type commitments: list
         :return: dict of FoodRequestGroup object
         :rtype: dict
         :raises Exception: if FoodRequestGroup was not created successfully

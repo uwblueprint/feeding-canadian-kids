32b67f67e0a9fad5016f4e820e86bfb78f0331ba
AryanAb <aryanabed48@gmail.com>
2022-11-08 17:18:41 -0500

    Fixed formatting

--- backend/app/graphql/all_users.py
+++ backend/app/graphql/all_users.py
@@ -1,25 +1,40 @@
 from ..services.implementations.user_service import UserService
 import graphene
-from .types import (
-    Query,
-    QueryList
-)
+import logging
+from .types import Query, QueryList
 
 
 class UserType(graphene.ObjectType):
     name = graphene.String()
     email = graphene.String()
-    phone = graphene.String()
     role = graphene.String()
 
 
 class AllUsersQuery(QueryList):
-    all_user = graphene.List(UserType, first=graphene.Int(default_value=5), offset=graphene.Int(default_value=0), role=graphene.String())
+    all_users = graphene.List(
+        UserType,
+        first=graphene.Int(default_value=5),
+        offset=graphene.Int(default_value=0),
+        role=graphene.String(default_value=""),
+    )
 
     def resolve_all_users(self, info, first, offset, role):
-        users = UserService.get_users()
+        user_service = UserService(logging.getLogger())
+        users = user_service.get_users()
 
-        if role:
-            return filter(lambda user: user.role == role, users)[offset:offset+first]
+        if role != "":
+            return [*filter(lambda user: user.role == role, users)][
+                offset : offset + first
+            ]
 
-        return users[offset:offset+first]
+        print(users)
+        return [
+            *map(
+                lambda user: UserType(
+                    name=f"{user['first_name']} {user['last_name']}",
+                    email=user["email"],
+                    role=user["role"],
+                ),
+                users[offset : offset + first],
+            )
+        ]

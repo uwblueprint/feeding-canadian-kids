91c9ef446de6fc788e09cdb3d6be0512f18c9b67
HeetShah <heetshah7642@gmail.com>
2022-11-22 22:54:10 -0500

    approving requets works

--- backend/tests/graphql/test_approve_onboarding_request.py
+++ backend/tests/graphql/test_approve_onboarding_request.py
@@ -3,34 +3,15 @@ from app.graphql import schema as graphql_schema
 
 def test_create_onboarding_request():
     query_string = """mutation testApproveOnboardingRequest{
-                        approveOnboardingRequest(
-                            OnboardingRequestObject:
-                                {id: "5f9f1b1b9b9b9b9b9b9b9b9b",
-                                info:
-                                    {contactName: "Jane Doe",
-                                    contactEmail: "something@gmail.com",
-                                    contactPhone: "12345",
-                                    role: "ASP"
-                                    },
-                                dateSubmitted: "2020-10-29T20:00:00.000Z",
-                                status: "Pending"
-                                }
-                        )
-                        {
-                            onboardingRequest {
-                            id
-                            info {
-                                contactName,
-                                contactEmail,
-                                contactPhone,
-                                role
-                            }
-                            dateSubmitted
-                            status
+                        approveOnboardingRequest(OnboardingRequestID: "1"){
+                            onboardingRequest{
+                                id
                             }
                         }
-                        
                     }"""
+                    
+                    
+                    
 
 
 

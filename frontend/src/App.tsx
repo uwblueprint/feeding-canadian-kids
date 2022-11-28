5fd603e44b9c15bfc703177b856a8416ed64b2ea
Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
2022-11-28 03:28:50 -0500

    Move routes to separate file for SOC

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useMemo, useReducer, useState } from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes as RouterRoutes,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import AuthWall from "./components/auth/AuthWall";
-import Login from "./components/auth/Login";
-import Signup from "./components/auth/Signup";
-import CreatePage from "./components/pages/CreatePage";
-import Dashboard from "./components/pages/Dashboard";
-import Default from "./components/pages/Default";
-import DisplayPage from "./components/pages/DisplayPage";
-import EditTeamInfoPage from "./components/pages/EditTeamPage";
-import HooksDemo from "./components/pages/HooksDemo";
-import NotFound from "./components/pages/NotFound";
-import SimpleEntityCreatePage from "./components/pages/SimpleEntityCreatePage";
-import SimpleEntityDisplayPage from "./components/pages/SimpleEntityDisplayPage";
-import SimpleEntityUpdatePage from "./components/pages/SimpleEntityUpdatePage";
-import UpdatePage from "./components/pages/UpdatePage";
+import Routes from "./Routes";
 import AUTHENTICATED_USER_KEY from "./constants/AuthConstants";
-import * as Routes from "./constants/Routes";
 import AuthContext from "./contexts/AuthContext";
 import SampleContext, {
   DEFAULT_SAMPLE_CONTEXT,
@@ -58,46 +40,9 @@ const App = (): React.ReactElement => {
         value={dispatchSampleContextUpdate}
       >
         <AuthContext.Provider value={currentAuthContext}>
-          <Router>
-            <RouterRoutes>
-              <Route path={Routes.HOME_PAGE} element={<Default />} />
-              <Route path={Routes.LOGIN_PAGE} element={<Login />} />
-              <Route path={Routes.SIGNUP_PAGE} element={<Signup />} />
-              <Route path={Routes.DASHBOARD_PAGE} element={<AuthWall />}>
-                <Route path="" element={<Dashboard />} />
-                <Route
-                  path={Routes.CREATE_ENTITY_PAGE}
-                  element={<CreatePage />}
-                />
-                <Route
-                  path={Routes.UPDATE_ENTITY_PAGE}
-                  element={<UpdatePage />}
-                />
-                <Route
-                  path={Routes.DISPLAY_ENTITY_PAGE}
-                  element={<DisplayPage />}
-                />
-                <Route
-                  path={Routes.CREATE_SIMPLE_ENTITY_PAGE}
-                  element={<SimpleEntityCreatePage />}
-                />
-                <Route
-                  path={Routes.UPDATE_SIMPLE_ENTITY_PAGE}
-                  element={<SimpleEntityUpdatePage />}
-                />
-                <Route
-                  path={Routes.DISPLAY_SIMPLE_ENTITY_PAGE}
-                  element={<SimpleEntityDisplayPage />}
-                />
-                <Route
-                  path={Routes.EDIT_TEAM_PAGE}
-                  element={<EditTeamInfoPage />}
-                />
-                <Route path={Routes.HOOKS_PAGE} element={<HooksDemo />} />
-              </Route>
-              <Route path="*" element={<NotFound />} />
-            </RouterRoutes>
-          </Router>
+          <BrowserRouter>
+            <Routes />
+          </BrowserRouter>
         </AuthContext.Provider>
       </SampleContextDispatcherContext.Provider>
     </SampleContext.Provider>
---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Route, Routes as RouteContainer } from "react-router-dom";
+
+import AuthWall from "./components/auth/AuthWall";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import CreatePage from "./components/pages/CreatePage";
+import Dashboard from "./components/pages/Dashboard";
+import Default from "./components/pages/Default";
+import DisplayPage from "./components/pages/DisplayPage";
+import EditTeamInfoPage from "./components/pages/EditTeamPage";
+import HooksDemo from "./components/pages/HooksDemo";
+import NotFound from "./components/pages/NotFound";
+import SimpleEntityCreatePage from "./components/pages/SimpleEntityCreatePage";
+import SimpleEntityDisplayPage from "./components/pages/SimpleEntityDisplayPage";
+import SimpleEntityUpdatePage from "./components/pages/SimpleEntityUpdatePage";
+import UpdatePage from "./components/pages/UpdatePage";
+import * as Paths from "./constants/Routes";
+
+const Routes = (): React.ReactElement => (
+  <RouteContainer>
+    <Route path={Paths.HOME_PAGE} element={<Default />} />
+    <Route path={Paths.LOGIN_PAGE} element={<Login />} />
+    <Route path={Paths.SIGNUP_PAGE} element={<Signup />} />
+    <Route path={Paths.DASHBOARD_PAGE} element={<AuthWall />}>
+      <Route path="" element={<Dashboard />} />
+      <Route path={Paths.CREATE_ENTITY_PAGE} element={<CreatePage />} />
+      <Route path={Paths.UPDATE_ENTITY_PAGE} element={<UpdatePage />} />
+      <Route path={Paths.DISPLAY_ENTITY_PAGE} element={<DisplayPage />} />
+      <Route
+        path={Paths.CREATE_SIMPLE_ENTITY_PAGE}
+        element={<SimpleEntityCreatePage />}
+      />
+      <Route
+        path={Paths.UPDATE_SIMPLE_ENTITY_PAGE}
+        element={<SimpleEntityUpdatePage />}
+      />
+      <Route
+        path={Paths.DISPLAY_SIMPLE_ENTITY_PAGE}
+        element={<SimpleEntityDisplayPage />}
+      />
+      <Route path={Paths.EDIT_TEAM_PAGE} element={<EditTeamInfoPage />} />
+      <Route path={Paths.HOOKS_PAGE} element={<HooksDemo />} />
+    </Route>
+    <Route path="*" element={<NotFound />} />
+  </RouteContainer>
+);
+
+export default Routes;

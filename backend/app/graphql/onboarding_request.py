f6a278648b1f4e67054cdd6a6b9f5db6171b37f4
Jessie Peng <jessie.peng2010@gmail.com>
2023-01-07 22:36:25 -0500

    Get OnboardingRequests (#26)
    * add get onboarding request service methods
    
    feat resolve get methods
    
    lint
    
    lint
    
    * use mongodb filtering
    
    * lint
    
    * added multiple filters
    
    * moved pagination
    
    * Fixing PR Comments
    
    * Fix PR Comments and Fixes
    
    Co-authored-by: Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>
    
    Co-authored-by: Abubakar Bello <a4bello@uwaterloo.ca>
    Co-authored-by: Julian Dominguez-Schatz <julian.dominguezschatz@gmail.com>

--- backend/app/graphql/onboarding_request.py
+++ backend/app/graphql/onboarding_request.py
@@ -85,6 +85,56 @@ class OnboardingRequestQueries(QueryList):
         ]
 
 
+# Queries
+
+# Return object for queries
+class GetOnboardingRequest(Query):
+    contact_name = graphene.String()
+    contact_email = graphene.String()
+    contact_phone = graphene.String()
+    role = graphene.String()
+    date_submitted = graphene.DateTime()
+    status = graphene.String()
+
+
+class OnboardingRequestQueries(QueryList):
+    getAllOnboardingRequests = graphene.List(
+        GetOnboardingRequest,
+        number=graphene.Int(default_value=5),
+        offset=graphene.Int(default_value=0),
+        role=graphene.String(default_value=""),
+        status=graphene.String(default_value=""),
+    )
+    getOnboardingRequestById = graphene.List(
+        GetOnboardingRequest,
+        id=graphene.String(),
+    )
+
+    def resolve_getAllOnboardingRequests(self, info, number, offset, role, status):
+        requests = services["onboarding_request_service"].get_all_onboarding_requests(
+            number,
+            offset,
+            role,
+            status,
+        )
+        return requests
+
+    def resolve_getOnboardingRequestById(self, info, id):
+        request = services["onboarding_request_service"].get_onboarding_request_by_id(
+            id
+        )
+        return [
+            GetOnboardingRequest(
+                contact_name=request.contact_name,
+                contact_email=request.contact_email,
+                contact_phone=request.contact_phone,
+                role=request.role,
+                date_submitted=request.date_submitted,
+                status=request.status,
+            )
+        ]
+
+
 # Mutations
 
 

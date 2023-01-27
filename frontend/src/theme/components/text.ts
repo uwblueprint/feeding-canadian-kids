3c58b71654a1f3be95906568a7a1973a52bfa2c6
Alyssa Guo <alyssasuiyiguo@gmail.com>
2023-01-26 21:36:20 -0500

    added new fonts & variants

--- frontend/src/theme/components/text.ts
+++ frontend/src/theme/components/text.ts
@@ -1,7 +1,43 @@
 const Text = {
   baseStyle: {},
   sizes: {},
-  variants: {},
+  variants: {
+    "desktop-display-xl": {
+      fontFamily: "Dimbo",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "40px",
+      lineHeight: "140%",
+    },
+    "desktop-heading": {
+      fontFamily: "Inter",
+      fontStyle: "normal",
+      fontWeight: "700",
+      fontSize: "20px",
+      lineHeight: "150%",
+    },
+    "desktop-button-bold": {
+      fontFamily: "Inter",
+      fontStyle: "normal",
+      fontWeight: "700",
+      fontSize: "18px",
+      lineHeight: "24px",
+    },
+    "desktop-xs": {
+      fontFamily: "Inter",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "14px",
+      lineHeight: "150%",
+    },
+    "desktop-heading-6": {
+      fontFamily: "DM Sans",
+      fontStyle: "normal",
+      fontWeight: "400",
+      fontSize: "16px",
+      lineHeight: "21px",
+    },
+  },
 };
 
 export default Text;

ab648f7e8e4dfbbf1d981c449e7bc1df31ad3436
Aryan <47926823+AryanAb@users.noreply.github.com>
2023-02-23 19:05:25 -0500

    10 update user by (#34)
    * Add basic resolver for fetching all users
    
    * Added a return user type and added role filtering
    
    * fix big with user services
    
    * Fixed formatting
    
    * Delete user_query.py
    
    * Added resolver to get user by id
    
    * Fixed bug where get_users was assumed to return a list of dictionaries instead of UserDTOs
    
    * Added GraphQL API for fetching all users
    
    - Added a user type to be returned
    - Added a basic resolver
    - Added unit tests
    
    * fix user query
    
    * added update user mutation
    
    * Addressed PR review comments
    
    * Removed extra submodule
    
    * Fixed the unit tests
    
    * Updated requirements.txt
    
    * Added flake8 inline exceptions
    
    * Added unit test and refactored
    
    * Ran through linter
    
    * Added mutation for updting user by id
    
    * Add basic resolver for fetching all users
    
    * Added a return user type and added role filtering
    
    * Fixed formatting
    
    * fix big with user services
    
    * Delete user_query.py
    
    * Added resolver to get user by id
    
    * Fixed bug where get_users was assumed to return a list of dictionaries instead of UserDTOs
    
    * Added GraphQL API for fetching all users
    
    - Added a user type to be returned
    - Added a basic resolver
    - Added unit tests
    
    * fix user query
    
    * Addressed PR review comments
    
    * Removed extra submodule
    
    * Fixed the unit tests
    
    * Updated requirements.txt
    
    * Added flake8 inline exceptions
    
    * Added unit test and refactored
    
    * Ran through linter
    
    * Added mutation for updting user by id
    
    * added update user mutation
    
    * resolve rebase conflicts
    
    * resolve rebase conflicts
    
    * add: mutations
    
    * ran lint
    
    * run lint
    
    * Fix Lint Errors
    
    * Fix duplicate error
    
    ---------
    
    Co-authored-by: zhangandrew37 <zhang.andrew37@gmail.com>
    Co-authored-by: Abubakar Bello <a4bello@uwaterloo.ca>

---
+++ backend/app/graphql/user_queries.py
@@ -0,0 +1,61 @@
+import graphene
+from .services import services
+from .types import QueryList, Query
+
+
+class User(Query):
+    name = graphene.String()
+    email = graphene.String()
+    role = graphene.String()
+
+
+class UserQueries(QueryList):
+    users = graphene.List(
+        User,
+        first=graphene.Int(default_value=5),
+        offset=graphene.Int(default_value=0),
+        role=graphene.String(default_value=""),
+    )
+
+    user = graphene.Field(
+        User,
+        id=graphene.String(required=True),
+    )
+
+    def resolve_users(self, info, first, offset, role):
+        user_service = services["user_service"]
+        users = user_service.get_users()
+
+        if role != "":
+            filtered = []
+            for user in users:
+                if user.role == role:
+                    filtered.append(
+                        User(
+                            name=f"{user.first_name} {user.last_name}",
+                            email=user.email,
+                            role=user.role,
+                        )
+                    )
+            return filtered[offset : offset + first]  # noqa: E203
+
+        return [
+            *map(
+                lambda user: User(
+                    name=f"{user.first_name} {user.last_name}",
+                    email=user.email,
+                    role=user.role,
+                ),
+                users[offset : offset + first],  # noqa: E203
+            )
+        ]
+
+    def resolve_user(self, info, id):
+        user_service = services["user_service"]
+        user = user_service.get_user_by_id(id)
+
+        return User(
+            name=f"{user.first_name} {user.last_name}",
+            email=user.email,
+            role=user.role,
+        )

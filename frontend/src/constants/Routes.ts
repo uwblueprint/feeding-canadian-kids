974494b5598f1004b071319c6d27de80911079a0
Anmol Tyagi <74927598+anmoltyagi1@users.noreply.github.com>
2023-03-13 21:52:01 -0400

    Onboarding: Forgot Password UI (#61)
    * UI for forgot pwd page
    
    * Fix PR
    
    ---------
    
    Co-authored-by: Abubakar Bello <a4bello@uwaterloo.ca>

--- frontend/src/constants/Routes.ts
+++ frontend/src/constants/Routes.ts
@@ -4,6 +4,8 @@ export const LOGIN_PAGE = "/login";
 
 export const JOIN_PAGE = "/join";
 
+export const FORGOT_PASSWORD_PAGE = "/forgot-password";
+
 export const SIGNUP_PAGE = "/signup";
 
 export const RESET_PASSWORD_PAGE = "/reset-password";

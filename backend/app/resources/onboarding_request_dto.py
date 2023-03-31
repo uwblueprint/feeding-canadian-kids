f825ec45828d630e729de2f2e224011bded16402
Anson He <ansonjwhe@gmail.com>
2023-03-31 03:01:04 -0400

    update onboarding_request_dto to include id

--- backend/app/resources/onboarding_request_dto.py
+++ backend/app/resources/onboarding_request_dto.py
@@ -6,16 +6,26 @@ from .validate_utils import validate_userinfo
 class OnboardingRequestDTO:
     def __init__(
         self,
+        id,
         info,
         date_submitted,
         status,
     ):
+        self.id = id
         self.info = info
         self.date_submitted = date_submitted
         self.status = status
+        # error_list = self.validate()
+        # if len(error_list) > 0:
+        #     error_message = "\n".join(error_list)
+        #     self.logger.error(error_message)
+        #     raise Exception(error_message)
 
     def validate(self):
         error_list = validate_userinfo(self.info, [])
+
+        if type(self.id) is not str:
+            error_list.append("The id supplied is not a string.")
 
         if type(self.date_submitted) is not datetime.datetime:
             error_list.append("The date_submitted supplied is not a datetime object.")

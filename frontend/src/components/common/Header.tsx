adb39464a929631a565e222fc191d10986a62712
Anson He <ansonjwhe@gmail.com>
2023-06-25 20:23:54 -0400

    hide user settings behind auth wall

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,13 +1,15 @@
 import { Button, Flex, Image } from "@chakra-ui/react";
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Gear from "../../assets/greenGear.svg";
 import Logo from "../../assets/logo.png";
 import { HOME_PAGE, SETTINGS_PAGE } from "../../constants/Routes";
+import AuthContext from "../../contexts/AuthContext";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { authenticatedUser } = useContext(AuthContext);
 
   return (
     <Flex
@@ -28,30 +30,32 @@ const Header = () => {
           cursor: "pointer",
         }}
       />
-      <Flex flexDir="row" gap="24px">
-        <Button
-          width={{ base: "30px", md: "60px" }}
-          height={{ base: "30px", md: "40px" }}
-          p="0"
-          bgColor="background.grey"
-          border="2px solid"
-          borderColor="primary.green"
-          _hover={{
-            color: "primary.green",
-            bgColor: "background.grey",
-          }}
-          onClick={() => {
-            navigate(SETTINGS_PAGE);
-          }}
-        >
-          <Image
-            width={{ base: "18px", md: "24px" }}
-            src={Gear}
-            alt="User Settings Button"
-          />
-        </Button>
-        {/* TODO: add Logout button */}
-      </Flex>
+      {authenticatedUser && (
+        <Flex flexDir="row" gap="24px">
+          <Button
+            width={{ base: "30px", md: "60px" }}
+            height={{ base: "30px", md: "40px" }}
+            p="0"
+            bgColor="background.grey"
+            border="2px solid"
+            borderColor="primary.green"
+            _hover={{
+              color: "primary.green",
+              bgColor: "background.grey",
+            }}
+            onClick={() => {
+              navigate(SETTINGS_PAGE);
+            }}
+          >
+            <Image
+              width={{ base: "18px", md: "24px" }}
+              src={Gear}
+              alt="User Settings Button"
+            />
+          </Button>
+          {/* TODO: add Logout button */}
+        </Flex>
+      )}
     </Flex>
   );
 };

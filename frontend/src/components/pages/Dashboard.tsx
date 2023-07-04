02b548c97697d973cd0bc4a574790c5873da5db6
hannahxdu <76927277+hannahxdu@users.noreply.github.com>
2023-07-04 18:42:35 -0400

    formatting

--- frontend/src/components/pages/Dashboard.tsx
+++ frontend/src/components/pages/Dashboard.tsx
@@ -1,49 +1,37 @@
-
-import { 
-    CalendarIcon,
-    ChevronDownIcon,
-    HamburgerIcon
-} from '@chakra-ui/icons';
-
+import { CalendarIcon, ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
 import {
-    Box,
-    Button,
-    Tab,
-    Table,
-    TableContainer,
-    TabList,
-    TabPanel,
-    TabPanels,
-    Tabs,
-    Tbody,
-    Text,
-    Td,
-    Tfoot,
-    Th,
-    Thead,
-    Tr,
-    Wrap
-} from '@chakra-ui/react';
-
-import React from 'react';
-
+  Box,
+  Button,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Table,
+  TableContainer,
+  Tabs,
+  Tbody,
+  Td,
+  Text,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+  Wrap,
+  useMediaQuery,
+} from "@chakra-ui/react";
 // eslint-disable-next-line import/order
 import dayGridPlugin from "@fullcalendar/daygrid";
 // eslint-disable-next-line import/order
 import FullCalendar from "@fullcalendar/react";
-
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import MealRequestForm from "./MealRequestForm";
 
 import BackgroundImage from "../../assets/background.png";
-
 import * as Routes from "../../constants/Routes";
-
 import SampleContext from "../../contexts/SampleContext";
-
 import Logout from "../auth/Logout";
-
 import RefreshCredentials from "../auth/RefreshCredentials";
 
 type ButtonProps = { text: string; path: string };
@@ -53,139 +41,153 @@ const NavButton = ({ text, path }: ButtonProps) => {
   return <Button onClick={() => navigate(path)}>{text}</Button>;
 };
 
+const [isWebView] = useMediaQuery("(min-width: 62em)");
+
 function App() {
   return (
-      <Box margin="150px" textAlign="center">
-        <Text
-            pb={{ base: 1, md: 5 }}
-            pl={{ base: 1, md: 6 }}
-            pt={{ base: 2, md: 8 }}
-            variant={{ base: "mobile-display-xl", md: "desktop-display-xl" }}
-        >
-            Your Dashboard 
-        </Text>
+    <Box margin="150px" textAlign="center">
+      <Text
+        pb={{ base: 1, md: 5 }}
+        pl={{ base: 1, md: 6 }}
+        pt={{ base: 2, md: 8 }}
+        variant={{ base: "mobile-display-xl", md: "desktop-display-xl" }}
+      >
+        Your Dashboard
+      </Text>
 
-        <Text fontSize="xs" marginBottom="20px">
-            Use this page to see your upcoming food deliveries.
-        </Text>
+      <Text fontSize="xs" marginBottom="20px">
+        Use this page to see your upcoming food deliveries.
+      </Text>
 
-        {/* tabs */}
-        <Tabs colorScheme='black'>
-          <TabList>
-            <Tab>
-                <Text
-                    variant={{
-                        base: "mobile-heading",
-                        md: "desktop-heading",
-                    }}
-                >
-                <CalendarIcon boxSize={5} mr={2} />
-                Calendar
-                </Text>
-            </Tab>
-            <Tab>
-                <Text
-                    variant={{
-                        base: "mobile-heading",
-                        md: "desktop-heading",
-                    }}
-                >
-                <HamburgerIcon boxSize={5} mr={2} />
-                List
-                </Text>
-            </Tab>
-            <Tab>
-                <Text
-                    variant={{
-                        base: "mobile-heading",
-                        md: "desktop-heading",
-                    }}
-                >
-                Test Buttons
-                </Text>
-            </Tab>
-          </TabList>
+      {/* tabs */}
+      <Tabs colorScheme="black">
+        <TabList>
+          <Tab>
+            <Text
+              variant={{
+                base: "mobile-heading",
+                md: "desktop-heading",
+              }}
+            >
+              <CalendarIcon boxSize={5} mr={2} />
+              Calendar
+            </Text>
+          </Tab>
+          <Tab>
+            <Text
+              variant={{
+                base: "mobile-heading",
+                md: "desktop-heading",
+              }}
+            >
+              <HamburgerIcon boxSize={5} mr={2} />
+              List
+            </Text>
+          </Tab>
+          <Tab>
+            <Text
+              variant={{
+                base: "mobile-heading",
+                md: "desktop-heading",
+              }}
+            >
+              Test Buttons
+            </Text>
+          </Tab>
+        </TabList>
 
-          <TabPanels>
-            <TabPanel>
-            <FullCalendar
-      plugins={[ dayGridPlugin ]}
-      initialView="dayGridMonth"
-    />
-            </TabPanel>
-            
-            <TabPanel>
-                <TableContainer>
-                    <Table variant='simple'>
-                        <Thead>
-                        {/* should abstract the rows into a react component */}
-                        <Tr>
-                            <Th>Date Requested</Th>
-                            <Th>Time Requested</Th>
-                            <Th>Donor's Name</Th>
-                            <Th>Number of Meals</Th>
-                            <Th></Th>
-                        </Tr>
-                        </Thead>
-                        <Tbody>
-                        <Tr>
-                            <Td>Oct 27, 2022</Td>
-                            <Td>5:00-6:00 PM</Td>
-                            <Td>Harvey's</Td>
-                            <Td>5</Td>
-                            <Td>
-                                <ChevronDownIcon boxSize={6} />
-                            </Td>
-                        </Tr>
-                        <Tr>
-                            <Td>Oct 27, 2022</Td>
-                            <Td>5:00-6:00 PM</Td>
-                            <Td>Harvey's</Td>
-                            <Td>5</Td>
-                            <Td>
-                                <ChevronDownIcon boxSize={6} />
-                            </Td>
-                        </Tr>
-                        <Tr>
-                            <Td>Oct 27, 2022</Td>
-                            <Td>5:00-6:00 PM</Td>
-                            <Td>Harvey's</Td>
-                            <Td>5</Td>
-                            <Td>
-                                <ChevronDownIcon boxSize={6} />
-                            </Td>
-                        </Tr>
-                        </Tbody>
-                    </Table>
-                </TableContainer>
-            </TabPanel>
-            <TabPanel>
+        <TabPanels>
+          <TabPanel>
+            {isWebView && (
+              <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                height="600px"
+              />
+            )}
+          </TabPanel>
+
+          <TabPanel>
+            <TableContainer>
+              <Table variant="simple">
+                <Thead>
+                  {/* should abstract the rows into a react component */}
+                  <Tr>
+                    <Th>Date Requested</Th>
+                    <Th>Time Requested</Th>
+                    <Th>Donor's Name</Th>
+                    <Th>Number of Meals</Th>
+                    <Th></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td>Oct 27, 2022</Td>
+                    <Td>5:00-6:00 PM</Td>
+                    <Td>Harvey's</Td>
+                    <Td>5</Td>
+                    <Td>
+                      <ChevronDownIcon boxSize={6} />
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Oct 27, 2022</Td>
+                    <Td>5:00-6:00 PM</Td>
+                    <Td>Harvey's</Td>
+                    <Td>5</Td>
+                    <Td>
+                      <ChevronDownIcon boxSize={6} />
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td>Oct 27, 2022</Td>
+                    <Td>5:00-6:00 PM</Td>
+                    <Td>Harvey's</Td>
+                    <Td>5</Td>
+                    <Td>
+                      <ChevronDownIcon boxSize={6} />
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </TabPanel>
+          <TabPanel>
             <Wrap>
-        <Logout />
-        <RefreshCredentials />
-        <NavButton text="Create Entity" path={Routes.CREATE_ENTITY_PAGE} />
-        <NavButton text="Update Entity" path={Routes.UPDATE_ENTITY_PAGE} />
-        <NavButton text="Display Entities" path={Routes.DISPLAY_ENTITY_PAGE} />
-        <NavButton
-          text="Create Simple Entity"
-          path={Routes.CREATE_SIMPLE_ENTITY_PAGE}
-        />
-        <NavButton
-          text="Update Simple Entity"
-          path={Routes.UPDATE_SIMPLE_ENTITY_PAGE}
-        />
-        <NavButton
-          text="Display Simple Entities"
-          path={Routes.DISPLAY_SIMPLE_ENTITY_PAGE}
-        />
-        <NavButton text="Edit Team" path={Routes.EDIT_TEAM_PAGE} />
-        <NavButton text="Hooks Demo" path={Routes.HOOKS_PAGE} />
-        <MealRequestForm />
-        </Wrap>
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
+              <Logout />
+              <RefreshCredentials />
+              <NavButton
+                text="Create Entity"
+                path={Routes.CREATE_ENTITY_PAGE}
+              />
+              <NavButton
+                text="Update Entity"
+                path={Routes.UPDATE_ENTITY_PAGE}
+              />
+              <NavButton
+                text="Display Entities"
+                path={Routes.DISPLAY_ENTITY_PAGE}
+              />
+              <NavButton
+                text="Create Simple Entity"
+                path={Routes.CREATE_SIMPLE_ENTITY_PAGE}
+              />
+              <NavButton
+                text="Update Simple Entity"
+                path={Routes.UPDATE_SIMPLE_ENTITY_PAGE}
+              />
+              <NavButton
+                text="Display Simple Entities"
+                path={Routes.DISPLAY_SIMPLE_ENTITY_PAGE}
+              />
+              <NavButton text="Edit Team" path={Routes.EDIT_TEAM_PAGE} />
+              <NavButton text="Hooks Demo" path={Routes.HOOKS_PAGE} />
+              <MealRequestForm />
+            </Wrap>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Box>
   );
 }
 

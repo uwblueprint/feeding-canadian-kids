ff4daeff1d234ccf7e589e864a0fc112ab15dffb
Tudor <tudor.barsan46@gmail.com>
2023-07-25 19:09:10 -0400

    Added mobile logout button and functionality

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,28 +1,60 @@
+import { gql, useMutation } from "@apollo/client";
 import { ArrowBackIcon, HamburgerIcon, SettingsIcon } from "@chakra-ui/icons";
-import { Button, Divider, Flex, IconButton, Image, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  Divider,
+  Flex,
+  IconButton,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import authAPIClient from "../../APIClients/AuthAPIClient";
 import greenGear from "../../assets/greenGear.svg";
 import Logo from "../../assets/logo.png";
 import whiteGear from "../../assets/whiteGear.svg";
 import {
   DASHBOARD_PAGE,
   HOME_PAGE,
+  LOGIN_PAGE,
   SETTINGS_PAGE,
 } from "../../constants/Routes";
 import AuthContext from "../../contexts/AuthContext";
 import useIsWebView from "../../utils/useIsWebView";
 import Logout from "../auth/Logout";
 
+const LOGOUT = gql`
+  mutation Logout($userId: String!) {
+    logout(userId: $userId) {
+      success
+    }
+  }
+`;
 
 const Header = () => {
   const isWebView = useIsWebView();
-  const { authenticatedUser } = useContext(AuthContext);
+  const { authenticatedUser, setAuthenticatedUser } = useContext(AuthContext);
   const navigate = useNavigate();
   const [isHovered, setIsHovered] = useState(false);
 
-  
+  const [logout] = useMutation<{ logout: { success: boolean } }>(LOGOUT);
+
+  const onLogOutClick = async () => {
+    const success = await authAPIClient.logout(
+      String(authenticatedUser?.id),
+      logout,
+    );
+    if (success) {
+      setAuthenticatedUser(null);
+    }
+    navigate(LOGIN_PAGE);
+  };
+
   const headerDesktop = (): React.ReactElement => {
     return (
       <Flex
@@ -31,12 +63,7 @@ const Header = () => {
         padding="12px 24px"
         bgColor="background.grey"
       >
-        <Flex
-          flexDir="row"
-          height="70px"
-          gap="24px"
-          alignItems="center"
-        >
+        <Flex flexDir="row" height="70px" gap="24px" alignItems="center">
           <Image
             src={Logo}
             alt="Logo"
@@ -115,12 +142,7 @@ const Header = () => {
         padding="12px 24px"
         bgColor="background.grey"
       >
-        <Flex
-          flexDir="row"
-          height="50px"
-          gap="24px"
-          alignItems="center"
-        >
+        <Flex flexDir="row" height="50px" gap="24px" alignItems="center">
           <Image
             src={Logo}
             alt="Logo"
@@ -163,26 +185,23 @@ const Header = () => {
             <Menu>
               <MenuButton
                 as={IconButton}
-                aria-label='Options'
+                aria-label="Options"
                 icon={<HamburgerIcon boxSize={9} />}
                 color="gray.gray600"
                 bgColor="background.grey"
                 variant="desktop-button-bold"
               />
               <MenuList>
-                <MenuItem 
-                  icon={<SettingsIcon/>}
+                <MenuItem
+                  icon={<SettingsIcon />}
                   onClick={() => {
                     navigate(SETTINGS_PAGE);
                   }}
                 >
                   Settings
                 </MenuItem>
-                <MenuItem icon={<ArrowBackIcon/>}>
-                  Log Out 
-                </MenuItem>
-                <MenuItem >
-                  <Logout /> 
+                <MenuItem icon={<ArrowBackIcon />} onClick={onLogOutClick}>
+                  Log Out
                 </MenuItem>
               </MenuList>
             </Menu>
@@ -192,10 +211,7 @@ const Header = () => {
     );
   };
 
-
-  return (
-    isWebView ? headerDesktop() : headerMobile()
-  );
+  return isWebView ? headerDesktop() : headerMobile();
 };
 
 export default Header;

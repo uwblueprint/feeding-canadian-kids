302e0fac0845d85e0e5b0dbe943c8c33c45f49fb
William Park <w22park@uwaterloo.ca>
2023-11-01 11:21:29 -0700

    Fix failing meal request test

--- backend/tests/graphql/test_meal_request.py
+++ backend/tests/graphql/test_meal_request.py
@@ -1,46 +1,52 @@
+from app.graphql import schema as graphql_schema
+
 """
 Tests for MealRequestchema and query/mutation logic
 Running graphql_schema.execute(...) also tests the service logic
 """
 
 
-def test_create_meal_request(graphql_schema):
-    mutation = """
-    mutation testCreateMealRequest {
+def test_create_meal_request(user_setup):
+    requestor, _, _ = user_setup
+
+    mutation = f"""
+    mutation testCreateMealRequest {{
       createMealRequest(
         deliveryInstructions: "Leave at front door",
         description: "Meal requests for office employees",
         dropOffLocation: "123 Main Street",
         dropOffTime: "16:30:00Z",
-        mealInfo: {portions: 40,
+        mealInfo: {{
+          portions: 40,
           dietaryRestrictions: "7 gluten free, 7 no beef",
-          mealSuggestions: "Burritos"},
+          mealSuggestions: "Burritos"}},
         onsiteStaff: [
-          {name: "John Doe", email: "john.doe@example.com", phone: "+1234567890"},
-          {name: "Jane Smith", email: "jane.smith@example.com", phone: "+9876543210"}],
-        requestorId: "507f1f77bcf86cd799439011",
+          {{name: "John Doe", email: "john.doe@example.com", phone: "+1234567890"}},
+          {{name: "Jane Smith", email: "jane.smith@example.com", phone: "+9876543210"}}],
+        requestorId: "{str(requestor.id)}",
         requestDates: [
             "2023-06-01",
             "2023-06-02",
         ],
       )
-      {
-        mealRequests {
+      {{
+        mealRequests {{
           status
           description
           id
           dropOffDatetime
-          mealInfo {
+          mealInfo {{
             portions
             dietaryRestrictions
             mealSuggestions
-          }
-        }
-      }
-    }
+          }}
+        }}
+      }}
+    }}
   """
 
     result = graphql_schema.execute(mutation)
+    # print(result)
 
     assert result.errors is None
     assert result.data["createMealRequest"]["mealRequests"][0]["status"] == "Open"
@@ -74,44 +80,47 @@ def test_create_meal_request(graphql_schema):
     )
 
 
-def test_get_meal_request_failure(graphql_schema):
-    mutation = """
-    mutation testCreateMealRequest {
+def test_get_meal_request_failure(user_setup):
+    requestor, _, _ = user_setup
+
+    mutation = f"""
+    mutation testCreateMealRequest {{
       createMealRequest(
         deliveryInstructions: "Leave at front door",
         description: "Meal requests for office employees",
         dropOffLocation: "123 Main Street",
         dropOffTime: "12:00:00Z",
-        mealInfo: {portions: 40,
+        mealInfo: {{
+          portions: 40,
           dietaryRestrictions: "7 gluten free, 7 no beef",
-          mealSuggestions: "Burritos"},
+          mealSuggestions: "Burritos"}},
         onsiteStaff: [
-          {name: "John Doe", email: "john.doe@example.com", phone: "+1234567890"},
-          {name: "Jane Smith", email: "jane.smith@example.com", phone: "+9876543210"}],
-        requestorId: "507f1f77bcf86cd799439011",
+          {{name: "John Doe", email: "john.doe@example.com", phone: "+1234567890"}},
+          {{name: "Jane Smith", email: "jane.smith@example.com", phone: "+9876543210"}}],
+        requestorId: "{str(requestor.id)}",
         requestDates: [
             "2023-06-01",
             "2023-06-02",
         ],
       )
-      {
-        mealRequests {
+      {{
+        mealRequests {{
           status
           description
           id
-          mealInfo {
+          mealInfo {{
             portions
             dietaryRestrictions
             mealSuggestions
-          }
-          requests {
+          }}
+          requests {{
             id
             donationDate
             status
-          }
-        }
-      }
-    }
+          }}
+        }}
+      }}
+    }}
     """
     result = graphql_schema.execute(mutation)
     result.errors is not None

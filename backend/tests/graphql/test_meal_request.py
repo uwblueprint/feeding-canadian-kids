c2e73a6aa83c80844f401a0c8baf23042a1549a5
William Park <w22park@uwaterloo.ca>
2023-11-13 18:47:13 -0800

    Improvements to meal request tests

--- backend/tests/graphql/test_meal_request.py
+++ backend/tests/graphql/test_meal_request.py
@@ -1,4 +1,5 @@
 from app.graphql import schema as graphql_schema
+from app.models.meal_request import MealRequest
 
 """
 Tests for MealRequestchema and query/mutation logic
@@ -6,8 +7,8 @@ Running graphql_schema.execute(...) also tests the service logic
 """
 
 
-def test_create_meal_request(user_setup):
-    requestor, _, _ = user_setup
+def test_create_meal_request(meal_request_setup):
+    requestor, _, _ = meal_request_setup
 
     mutation = f"""
     mutation testCreateMealRequest {{
@@ -88,60 +89,13 @@ def test_create_meal_request(user_setup):
         == "2023-06-02T16:30:00+00:00"
     )
 
+    # Delete the created meal requests
+    for meal_request in result.data["createMealRequest"]["mealRequests"]:
+        MealRequest.objects(id=meal_request["id"]).delete()
 
-def test_update_meal_request(user_setup):
-    requestor, _, _ = user_setup
-    create_meal_request_mutation = f"""
-    mutation testCreateMealRequest {{
-      createMealRequest(
-        deliveryInstructions: "Leave at front door",
-        description: "Meal requests for office employees",
-        dropOffLocation: "123 Main Street",
-        dropOffTime: "16:30:00Z",
-        mealInfo: {{
-          portions: 40,
-          dietaryRestrictions: "7 gluten free, 7 no beef",
-          mealSuggestions: "Burritos"
-        }},
-        onsiteStaff: [
-          {{
-            name: "John Doe",
-            email: "john.doe@example.com",
-            phone: "+1234567890"
-          }},
-          {{
-            name: "Jane Smith",
-            email: "jane.smith@example.com",
-            phone: "+9876543210"
-          }}
-        ],
-        requestorId: "{str(requestor.id)}",
-        requestDates: [
-            "2023-06-01",
-            "2023-06-02",
-        ],
-      )
-      {{
-        mealRequests {{
-          status
-          description
-          id
-          dropOffDatetime
-          mealInfo {{
-            portions
-            dietaryRestrictions
-            mealSuggestions
-          }}
-        }}
-      }}
-    }}
-  """
 
-    create_meal_request_result = graphql_schema.execute(create_meal_request_mutation)
-    print(create_meal_request_result)
-    created_meal_request_id = create_meal_request_result.data["createMealRequest"][
-        "mealRequests"
-    ][0]["id"]
+def test_update_meal_request(meal_request_setup):
+    _, _, meal_request = meal_request_setup
 
     updatedDateTime = "2023-10-31T16:45:00+00:00"
     updatedDescription = "Updated description"
@@ -159,7 +113,7 @@ def test_update_meal_request(user_setup):
     mutation = f"""
     mutation testUpdateMealRequest{{
       updateMealRequest(
-        mealRequestId:"{created_meal_request_id}",
+        mealRequestId:"{meal_request.id}",
         deliveryInstructions:"{updatedDeliveryInstructions}",
         description: "{updatedDescription}",
         dropOffDatetime: "{updatedDateTime}",
@@ -218,8 +172,8 @@ def test_update_meal_request(user_setup):
     assert updatedMealRequest["dropOffDatetime"] == updatedDateTime
 
 
-def test_get_meal_request_failure(user_setup):
-    requestor, _, _ = user_setup
+def test_get_meal_request_failure(meal_request_setup):
+    requestor, _, _ = meal_request_setup
 
     mutation = f"""
     mutation testCreateMealRequest {{
@@ -272,3 +226,48 @@ def test_get_meal_request_failure(user_setup):
     """
     result = graphql_schema.execute(mutation)
     result.errors is not None
+
+
+def test_get_meal_request_by_requestor_id(meal_request_setup):
+    requestor, _, meal_request = meal_request_setup
+
+    executed = graphql_schema.execute(
+        f"""{{
+          getMealRequestsByRequestorId(requestorId: "{str(requestor.id)}") {{
+            id
+            requestor {{
+              id
+            }},
+            description,
+            status,
+            dropOffDatetime,
+            dropOffLocation,
+            mealInfo {{
+              portions
+              dietaryRestrictions
+              mealSuggestions
+            }},
+            onsiteStaff {{
+              name
+              email
+              phone
+            }},
+            dateCreated,
+            dateUpdated,
+            deliveryInstructions,
+            donationInfo {{
+              donor {{
+                id
+              }},
+              commitmentDate
+              mealDescription
+              additionalInfo
+            }}
+          }}
+      }}"""
+    )
+
+    assert len(executed.data["getMealRequestsByRequestorId"]) == 1
+    result = executed.data["getMealRequestsByRequestorId"][0]
+    assert result["requestor"]["id"] == str(requestor.id)
+    assert result["id"] == str(meal_request.id)

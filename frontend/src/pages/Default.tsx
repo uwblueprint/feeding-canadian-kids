29e6fc2d8adc717e30cfb79825fc068e42815acb
Shahan Neda <shahan.neda@gmail.com>
2024-02-17 19:04:29 -0500

    Added test admin

--- frontend/src/pages/Default.tsx
+++ frontend/src/pages/Default.tsx
@@ -5,29 +5,43 @@ import { Link as RouterLink } from "react-router-dom";
 import BackgroundImage from "../assets/background.png";
 import * as Routes from "../constants/Routes";
 import AuthContext from "../contexts/AuthContext";
+import useIsAdmin from "../utils/useIsAdmin";
 import useIsMealDonor from "../utils/useIsMealDonor";
 
 const Default = (): React.ReactElement => {
   const isMealDonor = useIsMealDonor();
-  return (
-    <div
-      style={{
-        height: "100vh",
-        backgroundImage: `url(${BackgroundImage})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
-      {isMealDonor ? (
+  const isAdmin = useIsAdmin();
+
+  const wrapperStyles = {
+    height: "100vh",
+    backgroundImage: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  };
+  if (isMealDonor) {
+    return (
+      <div style={wrapperStyles}>
         <Link as={RouterLink} to={Routes.MEAL_DONOR_DASHBOARD_PAGE}>
           <Button>Go to meal donor dashboard</Button>
         </Link>
-      ) : (
-        <Link as={RouterLink} to={Routes.ASP_DASHBOARD_PAGE}>
-          <Button>Go to ASP dashboard</Button>
-        </Link>
-      )}
+      </div>
+    );
+  }
+
+  if (isAdmin) {
+    return (
+      <div style={wrapperStyles}>
+        <Button> Go to Admin Dashboard (unimplemented)</Button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={wrapperStyles}>
+      <Link as={RouterLink} to={Routes.ASP_DASHBOARD_PAGE}>
+        <Button>Go to ASP dashboard</Button>
+      </Link>
     </div>
   );
 };

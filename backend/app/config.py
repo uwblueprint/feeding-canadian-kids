cb4dd47068f7ce2f72cae4e93a3ed45f5c30180c
Colin Toft <cwt1078@gmail.com>
2024-06-01 22:31:55 -0400

    Use MG_DATABASE instead of MG_DB for consistency

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,7 +9,7 @@ class Config(object):
     # put any configurations here that are common across all environments
     # list of available configs: https://flask.palletsprojects.com/en/1.1.x/config/
     MONGODB_URL = os.getenv("MG_DATABASE_URL")
-    MONGODB_DB_NAME = os.getenv("MG_DB_NAME")
+    MONGODB_DB_NAME = os.getenv("MG_DATABASE_NAME")
 
 
 class DevelopmentConfig(Config):

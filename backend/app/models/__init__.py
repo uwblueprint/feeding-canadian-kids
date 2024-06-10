7344358605fbdf354e7138c3d588478296254ea3
Shahan Neda <shahan.neda@gmail.com>
2024-06-09 20:30:10 -0700

    Added logs to startup for debugging backend env

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from mongoengine import connect
 import mongomock
 import pymongo
+import os
 
 
 def init_app(app):
@@ -11,6 +12,13 @@ def init_app(app):
         if "USE_MONGOMOCK_CLIENT" in app.config
         else pymongo.MongoClient
     )
+    print("doing this!!")
+    print("MG_DATABASE_URL:", os.getenv("MG_DATABASE_URL"))
+    print("app.config mgdb url", app.config["MONGODB_URL"])
+    print("app.config mg db name", app.config["MONGODB_DB_NAME"])
+    print("app.config", app.config)
+    print("mongodb url in" , "MONGODB_URL" in app.config )
+    print("mongodb db name in" , "MONGODB_DB_NAME" in app.config )
     if "MONGODB_URL" in app.config and "MONGODB_DB_NAME" in app.config:
         connect(
             host=app.config["MONGODB_URL"],
@@ -18,4 +26,5 @@ def init_app(app):
             db=app.config["MONGODB_DB_NAME"],
         )
     else:
+        print("MG_DATABASE_URL:", os.getenv("MG_DATABASE_URL"))
         raise Exception("MG_DATABASE_URL and MG_DB_NAME must be set in the env file.")

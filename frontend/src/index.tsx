a3b301021f7ff3c9fff3684b1918bf4b3ba7d16e
Shahan Neda <shahan.neda@gmail.com>
2024-08-18 19:06:19 -0700

    Add sentry to backend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -33,7 +33,7 @@ Sentry.init({
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   tracePropagationTargets: ["localhost"],
   // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 

f7a14690925f2c527db8fd9b2ade25f69e209fa1
Colin Toft <cwt1078@gmail.com>
2024-08-24 10:31:56 -0400

    Clean up onboarding request page

--- frontend/src/components/asp/requests/TitleSection.tsx
+++ frontend/src/components/asp/requests/TitleSection.tsx
@@ -3,42 +3,29 @@ import React from "react";
 
 interface TitleSectionProps {
   title: string;
-  description: string;
 }
 
-const TitleSection = ({
-  title,
-  description,
-}: TitleSectionProps): React.ReactElement => (
-    <div>
-      <VStack
-        spacing={4}
-        alignItems="center"
-        height="fit-content"
-        style={{
-          background: "white",
-        }}
-        padding={{ base: "1rem", lg: "2rem" }}
+const TitleSection = ({ title }: TitleSectionProps): React.ReactElement => (
+  <div>
+    <VStack
+      spacing={4}
+      alignItems="center"
+      height="fit-content"
+      style={{
+        background: "white",
+      }}
+      padding={{ base: "1rem", lg: "2rem" }}
+    >
+      <Text
+        alignSelf={{ base: "center", lg: "unset" }}
+        variant="desktop-display-xl"
+        color="primary.blue"
+        as="b"
       >
-        <Text
-          alignSelf={{ base: "center", lg: "unset" }}
-          variant="desktop-display-xl"
-          color="primary.blue"
-          as="b"
-        >
-          {title}
-        </Text>
-
-        <Text
-          alignSelf={{ base: "center", lg: "unset" }}
-          variant="desktop-body"
-          textAlign="center"
-          maxWidth="100%"
-        >
-          {description}
-        </Text>
-      </VStack>
-    </div>
-  );
+        {title}
+      </Text>
+    </VStack>
+  </div>
+);
 
 export default TitleSection;
